aa503b57a26f36e3d435ddc78944d9033f5fb6e7
Ludovico Latini <latiniludovico@gmail.com>
2024-02-23 16:55:32 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 fetchers=( "foto.txt" "foto2.txt" "pepe.txt" "pepe2.txt" "shreck.txt" "flip.txt" "bear.txt" "culo.txt" "nyan.txt" "bruh" "kekw" "trollge")
 
 randomIndex=$((RANDOM % ${#fetchers[@]}))
-neofetch --ascii ./Documenti/ascii/"${fetchers[randomIndex]}"
+neofetch --ascii <your path>/"${fetchers[randomIndex]}"
